68dafb7528846978be79e9f4a5cf2c61eadd07a9
Erica Sanchez <ericazach@gmail.com>
2024-10-10 10:34:21 +0200

    add signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import { Home } from "./_routes/pages";
+import RootLayout from "./_routes/RootLayout";
 
 function App() {
-  return <div className="text-white">App</div>;
+  return (
+    <main className="flex h-screen ">
+      <Routes>
+        {/* PUBLUC ROUTES */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* PRIVATE ROUTES */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 chars.",
+  }),
+  username: z.string().min(2, {
+    message: "Username must be at least 2 chars.",
+  }),
+  email: z.string().email({
+    message: "Invalid email address.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 chars.",
+  }),
+});

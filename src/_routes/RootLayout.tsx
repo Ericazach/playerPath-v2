b1546b046d3fe4ca2b7a99190ba3ad612dfe2548
Erica Sanchez <ericazach@gmail.com>
2024-10-15 19:31:51 +0200

    fix for deployment

--- src/_routes/RootLayout.tsx
+++ src/_routes/RootLayout.tsx
@@ -1,6 +1,5 @@
-import BottomBar from "@/components/shared/BottomBar";
+// import BottomBar from "@/components/shared/BottomBar";
 import LeftSideBar from "@/components/shared/LeftSideBar";
-import MobileNav from "@/components/shared/MobileNav";
 import TopBar from "@/components/shared/TopBar";
 import { Outlet } from "react-router-dom";
 

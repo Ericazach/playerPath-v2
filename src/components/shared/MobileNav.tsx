ef841ef09e956b1af44ae4c581d61637c4b42e78
Erica Sanchez <ericazach@gmail.com>
2024-10-16 15:21:29 +0200

    fix hamburguer bar items

--- src/components/shared/MobileNav.tsx
+++ src/components/shared/MobileNav.tsx
@@ -7,10 +7,21 @@ import {
 import { sidebarLinks } from "@/constants";
 import { useUserContext } from "@/context/AuthContext";
 import { INavLink } from "@/types";
-import { Link, NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
 
 const MobileNav = () => {
   const { pathname } = useLocation();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess]);
 
   const { user } = useUserContext();
   return (
@@ -20,54 +31,64 @@ const MobileNav = () => {
           <img
             src="/assets/icons/hamburger.svg"
             alt="hamburger"
-            width={50}
-            height={50}
+            width={45}
+            height={45}
             className="cursor-pointer lg:hidden"
           />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-dark-2">
-          <Link to="/" className="flex gap-3 items-center mb-32">
-            <img
-              src="/assets/images/logo.png"
-              alt="logo"
-              width={60}
-              height={60}
-            />
-          </Link>
-          <ul className="flex flex-col gap-6">
-            {sidebarLinks.map((link: INavLink) => {
-              const isActive = pathname === link.route;
-
-              if (link.label === "Create Game" && user?.isAdmin === false) {
-                return null;
-              }
-
-              return (
-                <li
-                  key={link.label}
-                  className={`leftsidebar-link group ${
-                    isActive ? "bg-primary-500" : ""
-                  }`}
-                >
-                  <SheetClose asChild>
-                    <NavLink
-                      to={link.route}
-                      className="flex gap-4 items-center p-4"
+          <div className="flex flex-col justify-between h-full">
+            <div>
+              <Link to="/" className="flex gap-3 items-center mb-14">
+                <img
+                  src="/assets/images/logo.png"
+                  alt="logo"
+                  width={60}
+                  height={60}
+                />
+              </Link>
+              <ul className="flex flex-col gap-6">
+                {sidebarLinks.map((link: INavLink) => {
+                  const isActive = pathname === link.route;
+                  if (link.label === "Create Game" && user?.isAdmin === false) {
+                    return null;
+                  }
+                  return (
+                    <li
+                      key={link.label}
+                      className={`leftsidebar-link group ${
+                        isActive ? "bg-primary-500" : ""
+                      }`}
                     >
-                      <img
-                        src={link.imgURL}
-                        alt={link.label}
-                        className={`group-hover:invert-white ${
-                          isActive ? "invert-white" : ""
-                        }`}
-                      />
-                      {link.label}
-                    </NavLink>
-                  </SheetClose>
-                </li>
-              );
-            })}
-          </ul>
+                      <SheetClose asChild>
+                        <NavLink
+                          to={link.route}
+                          className="flex gap-4 items-center p-4"
+                        >
+                          <img
+                            src={link.imgURL}
+                            alt={link.label}
+                            className={`group-hover:invert-white ${
+                              isActive ? "invert-white" : ""
+                            }`}
+                          />
+                          {link.label}
+                        </NavLink>
+                      </SheetClose>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <Button
+              variant="ghost"
+              className="shad-button_ghost border-none"
+              onClick={() => signOut()}
+            >
+              <img src="/assets/icons/logout.svg" alt="logout" />
+              <p className="base-medium">Logout</p>
+            </Button>
+          </div>
         </SheetContent>
       </Sheet>
     </section>
--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
-import { Button } from "../ui/button";
-import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { Button } from "../ui/button";
+// import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 import { useUserContext } from "@/context/AuthContext";
 import MobileNav from "./MobileNav";
 
 const TopBar = () => {
-  const { mutate: signOut, isSuccess } = useSignOutAccount();
-  const navigate = useNavigate();
+  // const { mutate: signOut, isSuccess } = useSignOutAccount();
+  // const navigate = useNavigate();
   const { user } = useUserContext();
 
-  useEffect(() => {
-    if (isSuccess) {
-      navigate(0);
-    }
-  }, [isSuccess]);
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     navigate(0);
+  //   }
+  // }, [isSuccess]);
 
   return (
     <section className="topbar">
@@ -29,24 +29,25 @@ const TopBar = () => {
               height={60}
             />
           </Link>
-          <MobileNav />
         </div>
 
         <div className="flex gap-4">
-          <Button
+          {/* <Button
             variant="ghost"
             className="shad-button_ghost"
             onClick={() => signOut()}
           >
             <img src="/assets/icons/logout.svg" alt="logout" />
-          </Button>
-          <Link to={`/profile/${user?.id}`} className="flex-center gap-3">
+          </Button> */}
+
+          <Link to={`/profile/${user?.id}`} className="flex-center gap-3 ">
             <img
               src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
               alt="profile"
               className="w-8 h-8 rounded-full"
             />
           </Link>
+          <MobileNav />
         </div>
       </div>
     </section>
